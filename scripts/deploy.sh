badbbb7db2c7d4808c00cf69d67b7dc5dd8043d7
Angus-Warman <anguswarman@gmail.com>
2026-04-14 12:33:56 +0800

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,12 +8,14 @@ SCREEN_NAME=hello-world-complete
 TEMP="${DIR}${EXEC_NAME}-TEMP"
 FINAL="${DIR}${EXEC_NAME}"
 
+rm -f $TEMP
 echo "Downloading latest release..."
 curl --no-progress-meter -L $SOURCE_URL -o $TEMP
 echo "Closing previous version..."
-pkill -f EXEC_NAME || true
+pkill -f "$EXEC_NAME" || true
 mv -f $TEMP $FINAL
 chmod +x $FINAL
 echo "Starting server..."
+cd $DIR # Go to folder to get correct .env
 screen -dmS $SCREEN_NAME $FINAL
 echo "Done"
